8cc2640f598880caaed1025facb83b0dcef86e9b
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-05 09:10:31 -0500

    Se modifica pruebas

--- microservicio/dominio/src/test/java/com/ceiba/compra/entidad/CompraTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/compra/entidad/CompraTest.java
@@ -1,41 +1,7 @@
 package com.ceiba.compra.entidad;
 
-import com.ceiba.BasePrueba;
-import com.ceiba.cliente.servicio.testdatabuilder.ClienteTestDataBuilder;
-import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
-import com.ceiba.cliente.modelo.entidad.Cliente;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class CompraTest {
 
 
-    @Test
-    @DisplayName("Deberia crear correctamente el cliente")
-    void deberiaCrearCorrectamenteElCliente() {
-
-        LocalDateTime fechaCreacion = LocalDateTime.now();
-        Cliente cliente = new ClienteTestDataBuilder().conFechaCreacionCliente(fechaCreacion).conIdCliente(1L).build();
-        assertEquals(1, cliente.getIdCliente());
-        assertEquals("jairo", cliente.getNombreCliente());
-        assertEquals(10, cliente.getTipoCliente());
-        assertEquals(fechaCreacion, cliente.getFechaCreacionCliente());
-    }
-
-    @Test
-    void deberiaFallarSinNombreDeCliente() {
-
-
-        ClienteTestDataBuilder clienteTestDataBuilder = new ClienteTestDataBuilder().conNombreCliente(null).conIdCliente(1L);
-
-        BasePrueba.assertThrows(() -> {
-                    clienteTestDataBuilder.build();
-                },
-                ExcepcionValorObligatorio.class, "Se debe ingresar el nombre de cliente");
-    }
-
 }
+
