a3e595c2b2cc8008007d1f6bfa44c01355b9deb4
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-05 08:54:57 -0500

    Se modifica sql

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,1 +1,3 @@
-insert into usuario(id, nombre,clave,fecha_creacion) values(1,'test','1234',now())
+insert into usuario(id, nombre,clave,fecha_creacion) values(1,'test','1234',now());
+
+insert into compra(idCompra, precio,idCliente,fecha_creacion) values(1,20000,10,now());
--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.2.__data.sql
+++
@@ -1,3 +0,0 @@
-insert into cliente(idCliente, nombre_cliente,tipo_cliente,fecha_creacion) values(1,'Jairo','10',now())
-
-insert into compra(idCompra, precio,clave,fecha_creacion) values(1,20000,'10',now())
