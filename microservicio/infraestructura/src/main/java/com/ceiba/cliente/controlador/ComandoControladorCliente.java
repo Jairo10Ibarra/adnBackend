21ddc239d29e0b14e537e792ef69753798d390e8
jairo.ibarra <jairo.ibarra@ceiba.com.co>
2022-04-17 08:30:22 -0500

    Se optimiza codigo.

--- microservicio/infraestructura/src/main/java/com/ceiba/cliente/controlador/ComandoControladorCliente.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/cliente/controlador/ComandoControladorCliente.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/cliente")
 @Api(tags = {"Controlador comando clientes"})
 public class ComandoControladorCliente {
 
